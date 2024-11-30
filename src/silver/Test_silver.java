27a3c8a02a9f1e1fff07e29bc4a8531fecfdbac0
SrihithChennareddy <SrihithChennareddy@10.0.0.189>
2024-11-30 11:26:53 -0800

    Test program for silver

---
+++ src/silver/Test_silver.java
@@ -0,0 +1,12 @@
+package silver;
+
+public class Test_silver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Test Silver");
+
+	}
+
+}
