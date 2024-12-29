d424ae77715169e83c912eaaca4232d27122731f
Srihith Chennareddy <srihith.chennareddy@gmail.com>
2024-12-29 15:23:32 -0800

    BinarySearch
    Basic Binary Search

---
+++ src/silver/BinarySearch.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+    
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        ArrayList<Integer> nums = new ArrayList<Integer>();
+        int x = 6;
+        while (st.hasMoreTokens()){  
+            nums.add(Integer.parseInt(st.nextToken()));
+        }
+        Collections.sort(nums);
+        System.out.println(nums);
+        System.out.println(binarySearch(nums,x));
+
+    }
+
+    public static Integer binarySearch(ArrayList<Integer> nums, Integer x){
+        int right = nums.size();
+        int left = 0;
+        while(left < right){
+            int mid = (left+right)/2;
+            if(x < nums.get(mid)){
+                right = mid ;
+            } else if(x > nums.get(mid)){
+                left = mid+1;
+            } else{
+                return mid;
+            }
+        }
+        return -1;
+
+    }
+    
+}
