87bbd243c77c9f5b0710ea0f2b0e6c0251629c22
SrihithChennareddy <SrihithChennareddy@10.0.0.189>
2025-01-03 21:34:36 -0800

    Updated Class name

--- src/silver/BinarySearch.java
+++ src/silver/BinarySearch.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class BinarySearch {
     public static void main(String[] args) throws IOException {
     
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
--- src/silver/USACO_Silver_Dec2016_Problem1.java
+++ src/silver/USACO_Silver_Dec2016_Problem1.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Problem1 {
+public class USACO_Silver_Dec2016_Problem1 {
     public static void main(String[] args) throws IOException {
     
         BufferedReader br = new BufferedReader(new FileReader("haybales.in"));
